a3c22875cce2cc9ad8e93df761d13b5d12870051
shenshuwen <1044042600@qq.com>
2018-07-12 11:51:16 +0800

    1.前面的都是废话，最主要swagger没有出现的原因是这个。你手动指定了controller的包，但是这个包并不存在。所有把这个删掉就好了，或者你手动指定你controller所在的包。

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -20,7 +20,6 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hexun.bdc.auth.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
